9671f8900240667863bf3decd39313bbaef6a488
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-05-01 11:26:30 +0200

    importato middleware per la gestione degli errori nell'app

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const moviesRouter = require("./routers/movies");
+//IMPORT IL MIDDLEWARE PER LA GESTIONE DEGLI ERRORI
+const errorsHandler = require("./middlewares/errorsHandler");
+app.use(errorsHandler);
 
+//IMPORTO IL ROUTER
+const moviesRouter = require("./routers/movies");
 app.use("/movies", moviesRouter);
 
 app.listen(port, () => {
