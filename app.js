035d91505665445cb441f36e67918805261da8f9
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-05-01 11:38:53 +0200

    introdotte variabili di ambiente

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 
-//IMPORT IL MIDDLEWARE PER LA GESTIONE DEGLI ERRORI
-const errorsHandler = require("./middlewares/errorsHandler");
-app.use(errorsHandler);
+const port = process.env.PORT;
 
-//IMPORTO IL ROUTER
+// IMPORTO IL ROUTER
 const moviesRouter = require("./routers/movies");
 app.use("/movies", moviesRouter);
+
+// IMPORT IL MIDDLEWARE PER LA GESTIONE DEGLI ERRORI
+const errorsHandler = require("./middlewares/errorsHandler");
+app.use(errorsHandler);
 
 app.listen(port, () => {
   console.log(`Connessione avvenuta con successo alla porta ${port}`);
