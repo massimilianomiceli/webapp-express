be610bab132bbe481239c0d4840a6e423a52d6e2
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-05-01 11:22:49 +0200

    corretto codice all interno del router

--- routers/movies.js
+++ routers/movies.js
@@ -1,11 +1,15 @@
 //IMPORTO EXPRESS
 const express = require("express");
+
 const moviesRouter = express.Router();
 
+//IMPORTO LE FUNZIONI DEL CONTROLLER
+const moviesController = require("../controllers/moviesController");
+
 //ROTTA INDEX
-moviesRouter.get("/", (req, res) => {});
+moviesRouter.get("/", moviesController.index);
 
 //ROTTA SHOW
-moviesRouter.get("/:id", (req, res) => {});
+moviesRouter.get("/:id", moviesController.show);
 
 module.exports = moviesRouter;
