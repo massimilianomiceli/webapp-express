c13ce8cf865b8fca225551a21a5f6f78b39bb2a3
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-05-01 12:28:19 +0200

    aggiunto middleware per le rotte insesistenti

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const port = process.env.PORT;
 
 // IMPORTO IL ROUTER
 const moviesRouter = require("./routers/movies");
-app.use("/movies", moviesRouter);
 
 // IMPORT IL MIDDLEWARE PER LA GESTIONE DEGLI ERRORI
 const errorsHandler = require("./middlewares/errorsHandler");
+
+//IMPORT IL MIDDLEWARE PER LE ROTTE INSESISTENTI
+const notFound = require("./middlewares/notFound");
+
+app.use("/movies", moviesRouter);
+
 app.use(errorsHandler);
+
+app.use(notFound);
 
 app.listen(port, () => {
   console.log(`Connessione avvenuta con successo alla porta ${port}`);
