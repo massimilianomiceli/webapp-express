15cdf5a3f324b83ca94b6d3d2d43ae0184049d5e
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-05-01 11:58:25 +0200

    implementata rotta index

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,6 +1,15 @@
+//CONNESSIONE AL DATABASE
 const connection = require("../data/db_movies");
 
-function index(req, res) {}
+function index(req, res) {
+  const sql = "SELECT * from movies";
+
+  connection.query(sql, (err, results) => {
+    if (err)
+      return res.status(500).json({ error: "Query al database fallita" });
+    res.json(results);
+  });
+}
 
 function show(req, res) {}
 
